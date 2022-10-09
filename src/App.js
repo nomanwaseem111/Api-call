b621ed1654f343f321ee08b1c7a59d1ca3186d1b
Noman Waseem <nomanwaseem111@gmail.com>
2022-10-09 07:57:29 +0500

    api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useState } from 'react';
+import { useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  
+  const [activities,setActivities] = useState([])
+  const [isLoading,isSetLoading] = useState(true)
+
+ 
+  let isLoad = () => {
+
+    isSetLoading(true)
+    fetch("http://www.boredapi.com/api/activity/")
+      .then((response) => response.json())
+      .then((data) => setActivities([...activities , data.activity]));
+      isSetLoading(false)
+  }
+
+  useEffect(() => { isLoad() },[])
+
+  if(isLoading){
+    return <h1>Loading...</h1>
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <ul>
+          {activities.map((elem,index) => {
+              return <li key={index}>{elem}</li>
+          })}
+      </ul>
+      <button onClick={isLoad}>Load Another</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
